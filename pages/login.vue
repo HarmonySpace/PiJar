970633cbd53df712b4f33c7b8c771c9e42525ba0
HarmonySpace <jchavaspace@gmail.com>
2024-11-18 00:06:58 -0600

    feat(view): implement login layout

--- pages/login.vue
+++ pages/login.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+definePageMeta({
+  layout: false,
+});
 const auth = useSupabaseClient().auth;
 const credentials = ref({
   email: "",
@@ -21,48 +24,38 @@ const handlePassword = (value: string) => {
 </script>
 
 <template>
-  <div
-    class="w-full min-h-screen flex justify-center md:justify-between items-center"
-  >
-    <div
-      class="basis-4/6 w-full h-screen bg-gradient-to-tr to-finn-500 via-medium-purple-950 from-neutral-950 rounded-e-[2rem]"
-    ></div>
-    <div
-      class="w-10/12 sm:basis-2/6 h-full flex flex-col items-center justify-center"
-    >
-      <div class="max-w-md space-y-8">
-        <div class="w-full flex flex-col items-center justify-center">
-          <p class="text-center text-xl text-finn-500">Oink!</p>
-          <h1 class="text-center text-6xl text-finn-100">Login</h1>
-          <br />
+  <NuxtLayout name="login">
+    <template #title>Acceder</template>
+    <template #form>
+      <form class="space-y-6" @submit.prevent="login()">
+        <InputTextFields
+          id="credentials_email"
+          label="Correo electrónico"
+          type="email"
+          required
+          placeholder="ejemplo@dominio.com"
+          @update="(value: string) => handleEmail(value)"
+        />
+        <InputTextFields
+          id="credentials_password"
+          label="Contraseña"
+          type="password"
+          required
+          placeholder="oink123"
+          @update="(value: string) => handlePassword(value)"
+        />
+        <div class="w-full">
+          <ButtonText class="mt-8 w-full" text="Acceder" variant="outlined" />
         </div>
-        <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-          <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-            <form class="space-y-6" @submit.prevent="login">
-              <InputTextFields
-                id="credentials_email"
-                label="Correo electrónico"
-                type="email"
-                required
-                placeholder="ejemplo@dominio.com"
-                @update="(value: string) => handleEmail(value)"
-              />
-              <InputTextFields
-                v-model="credentials.email"
-                id="credentials_name"
-                label="Contraseña"
-                type="password"
-                required
-                placeholder="oink123"
-                @update="(value: string) => handlePassword(value)"
-              />
-              <div>
-                <ButtonText class="w-full" text="Acceder" variant="outlined" />
-              </div>
-            </form>
-          </div>
+        <div class="w-full">
+          <p class="text-center text-xl text-finn-500">
+            ¿No tienes cuenta?
+            <a href="/register" class="text-finn-100 hover:text-finn-200"
+              >Registrate</a
+            >
+          </p>
         </div>
-      </div>
-    </div>
-  </div>
+      </form>
+    </template>
+  </NuxtLayout>
 </template>
---
+++ pages/register.vue
@@ -0,0 +1,86 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: false,
+});
+const auth = useSupabaseClient().auth;
+const credentials = ref({
+  email: "",
+  password: "",
+  passwordConfirm: "",
+});
+const correctPassword = ref(false);
+const register = async () => {
+  if (correctPassword.value) {
+    const { error } = await auth.signUp(credentials.value);
+    if (error) {
+      console.log(error);
+    } else {
+      return navigateTo("/");
+    }
+  } else {
+    console.log("Passwords do not match");
+  }
+};
+const handleEmail = (value: string) => {
+  credentials.value.email = value;
+};
+const handlePassword = (value: string) => {
+  credentials.value.password = value;
+};
+const handlePasswordConfirm = (value: string) => {
+  credentials.value.passwordConfirm = value;
+  if (credentials.value.password !== credentials.value.passwordConfirm) {
+    console.log("Passwords do not match");
+  } else {
+    correctPassword.value = true;
+  }
+};
+</script>
+
+<template>
+  <NuxtLayout
+    name="login"
+    bg_banner="bg-gradient-to-tr to-medium-purple-500 via-finn-950 from-neutral-950"
+  >
+    <template #title>Registrarse</template>
+    <template #form>
+      <form class="space-y-6" @submit.prevent="register()">
+        <InputTextFields
+          id="credentials_email"
+          label="Correo electrónico"
+          type="email"
+          required
+          placeholder="ejemplo@dominio.com"
+          @update="(value: string) => handleEmail(value)"
+        />
+        <InputTextFields
+          id="credentials_password"
+          label="Contraseña"
+          type="password"
+          required
+          placeholder="oink123"
+          @update="(value: string) => handlePassword(value)"
+        />
+        <InputTextFields
+          id="credentials_password"
+          label="Confirmar contraseña"
+          type="password"
+          required
+          placeholder="oink123"
+          @update="(value: string) => handlePasswordConfirm(value)"
+        />
+        <div>
+          <ButtonText class="mt-8 w-full" text="Acceder" variant="outlined" />
+        </div>
+        <div class="w-full">
+          <p class="text-center text-xl text-finn-500">
+            ¿Ya tienes cuenta?
+            <a href="/login" class="text-finn-100 hover:text-finn-200"
+              >Inicia sesión</a
+            >
+          </p>
+        </div>
+      </form>
+    </template>
+  </NuxtLayout>
+</template>
