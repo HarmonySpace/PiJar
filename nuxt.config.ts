5e16c036a9101642c412edf28290994a1667e7e5
HarmonySpace <jchavaspace@gmail.com>
2024-11-26 06:44:11 -0600

    feat(store): add appwrite stores

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
-  devtools: { enabled: true },
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true,
+    },
+  },
+  ssr: false,
   modules: [
     "nuxt-lucide-icons",
     "@pinia/nuxt",
